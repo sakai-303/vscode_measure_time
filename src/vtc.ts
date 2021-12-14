02157692b0bff1aaf2c64a765bfc1a6b4c7bf315
hundo <44696693+hundo303@users.noreply.github.com>
2021-12-14 21:46:39 +0900

    [update] IDとパスワードの入力を実装

--- src/vtc.ts
+++ src/vtc.ts
@@ -1,9 +1,10 @@
-import { resolve } from "path";
+import { PassThrough } from 'stream';
+import * as vscode from 'vscode'
 
 export class Vtc {
     private writing_lang: string = '';
     // 単位はms
-    private start_time: number
+    private start_time:number
 
     constructor(){
         this.start_time = Date.now()
@@ -31,4 +32,25 @@ export class Vtc {
     private sleep(sec: number) {
         return new Promise(resolve => setTimeout(resolve, sec*1000))
     }
+}
+
+export async function auth() {
+    let id_option = {value: ""}
+    await vscode.window.showInputBox(id_option).then(
+        async (value?: string) => {
+            if (value == "" || value == undefined){
+                return
+            }
+            let pass_option = {value: "", password: true}
+            await vscode.window.showInputBox(pass_option).then()
+        }
+    )
+}
+
+function set_id(value: string){
+    // iikanzi
+}
+
+function set_pass(value?: string){
+    // iikanzi
 }
